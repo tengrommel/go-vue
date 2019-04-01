644359de723e80ebb763af34c1adbbdad7f6f123
tengrommel <wwwsousoucom@126.com>
2019-04-02 00:53:39 +0800

    other commit

--- scripts/deploy-dev.sh
+++ scripts/deploy-dev.sh
@@ -3,7 +3,7 @@
 if [ -z ${SHA+x} ]; then SHA=$(git rev-parse HEAD); fi
 
 docker build -t wwwsousoucom/echo1:lastest-dev -f ./echoservers/echo1/Dockerfile.dev ./echoservers/echo1
-docker build -t wwwsousoucom/echo2:$SHA -f ./echoservers/echo1/Dockerfile.dev ./echoservers/echo1
+docker build -t wwwsousoucom/echo1:$SHA -f ./echoservers/echo1/Dockerfile.dev ./echoservers/echo1
 
 docker push wwwsousoucom/echo1:lastest-dev
-docker push wwwsousoucom/echo2:$SHA
+docker push wwwsousoucom/echo1:$SHA
